b711d8cc210134fbf10b06b7ec48d87f0b5c09c1
Dave Alvarado <hsc.coach@gmail.com>
2020-11-03 14:22:12 -0500

    11/3/20 2:21PM

--- src/js/DoutMaker.js
+++ src/js/DoutMaker.js
@@ -1,67 +1,60 @@
 class DonutMaker {
-  constructor() {
-    this._donutCount = 0;
-    this._autoClickerCost = 100;
-    this._numberOfAutoClickers = 0;
-  }
-
-  stashDonutsforTesting() {
-    this._donutCOunt += 1000;
-  }
-  recordClick() {
-    this._donutCount++;
-  }
-  get clickCount() {
-    return this._donutCount;
-  }
-  recordAutoClick() {
-    this._donutCount++;
-  }
-  get autoClickCount() {
-    return this._donutCount;
-  }
-
-  get autoClickerCost() {
-    let unRoundedCost = 100 * Math.pow(1.1, this._numberOfAutoClickers - 1);
-    this._autoClickerCost = Math.round(unRoundedCost);
-    return this._autoClickerCost;
-  }
-
-  buyAutoClicker() {
-    if (this._donutCount >= this._autoClickerCost) {
-      this._donutCount -= this._autoClickerCost;
-      this._donutCount++;
-    } else {
-      this._donutCount = this._donutCount;
-      // let them know they cant buy a auto clicker yet
+    constructor() {
+        this._donutCount = 0;
+        this._numberOfAutoClickers = 0;
+        this._autoClickerCost = 100;
     }
-  }
 
-  buyAutoClicker() {
-    if (this.autoClickersPurchased == 0) {
-      if (this._donutCount >= 100) {
-        this._numberOfAutoClickers++;
-        this._donutCount -= 100;
-      } else {
-      }
-    } else {
-      if (this._donutCount >= this.autoClickerCost) {
-        this._numberOfAutoClickers++;
-        this._donutCount -= this.autoClickerCost;
-      } else {
-      }
+    stashDonutsforTesting() { //Abbys Idea for Tests
+        this._donutCount += 1000;
     }
-  }
 
-  get autoClickersPurchased() {
-    return this._numberOfAutoClickers;
-  }
-
-  activateAutoClickers() {
-    var i = 0;
-    while (i < this._numberOfAutoClickers) {
-      this._donutCount++;
-      i++;
+    recordClick() {
+        this._donutCount++;
     }
-  }
+    get clickCount() {
+        return this._donutCount;
+    }
+    recordAutoClick() {
+        this._donutCount++;
+    }
+    get autoClickCount() {
+        return this._donutCount;
+    }
+
+
+    get autoClickerCost() {
+        let unRoundedCost = 100 * Math.pow(1.1, this._numberOfAutoClickers - 1);
+        this._autoClickerCost = Math.round(unRoundedCost)
+        return this._autoClickerCost;
+
+    }
+
+    buyAutoClicker() {
+        if (this.autoClickersPurchased == 0) {
+            if (this._donutCount >= 100) {
+                this._numberOfAutoClickers++;
+                this._donutCount -= 100;
+            } else {}
+        } else {
+            if (this._donutCount >= this.autoClickerCost) {
+                this._numberOfAutoClickers++;
+                this._donutCount -= this.autoClickerCost;
+            } else {}
+        }
+    }
+
+    get autoClickersPurchased() {
+        return this._numberOfAutoClickers;
+    }
+
+    activateAutoClickers() {
+        var i = 0
+        while (i < this._numberOfAutoClickers) {
+            this._donutCount++
+                i++;
+        }
+
+    }
+
 }
