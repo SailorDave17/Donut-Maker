cc6992d59381593eb8cecd38c8605a4cf68a85a7
Dave <HSC.Coach@gmail.com>
2020-11-06 14:47:19 -0500

    donut button working

--- src/js/app.js
+++ src/js/app.js
@@ -28,15 +28,40 @@ const displayClickValue = function(donut) {
 
 }
 
-const displayAutoClickerCount = function(donut) {
-    const autoClickerCountText = document.querySelector(".auto-clicker-count");
-    autoClickerCountText.innerText = donut.autoClickCount;
+const displayNumberOfAutoClickers = function(donut) {
+    const numberOfAutoClickersText = document.querySelector(".auto-clicker-count");
+    numberOfAutoClickersText.innerText = donut.autoClickCount;
 }
 
 const purchaseAutoClicker = document.querySelector(".auto-button");
 purchaseAutoClicker.addEventListener("click", () => {
     userDonut.purchaseAutoClicker();
     updateDonutCount(userDonut);
-    displayAutoClickerCount(userDonut)
+    displayNumberOfAutoClickers(userDonut)
     displayAutoClickerCost(userDonut);
 });
+
+const displayAutoClickerCost = function(donut) {
+    const autoClickerCostText = document.querySelector(".auto-clicker-cost");
+    autoClickerCostText.innerText = "Cost: " + donut.autoClickerCost + " Donuts";
+}
+
+const displayMultiplierCount = function(donut) {
+    const multiplierCountText = document.querySelector(".multiplier-count");
+    multiplierCountText.innerText = donut.multiplierCount;
+}
+
+const purchaseMultiplier = document.querySelector(".multiplier-button");
+purchaseMultiplier.addEventListener("click", () => {
+    userDonut.purchaseMultiplier();
+    updateDonutCount(userDonut);
+    displayMultiplierCount(userDonut);
+    displayMultiplierCost(userDonut);
+    displayClickValue(userDonut);
+});
+
+const displayMultiplierCost = function(donut) {
+    const MultiplierCostText = document.querySelector(".multiplier-cost");
+    MultiplierCostText.innerText = "Cost: " + Math.round(donut.multiplierCost) + " Donuts";
+}
+
