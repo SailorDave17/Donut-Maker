b042c00d2f3ec445ba28ebc4bc9d75b5ba6bbb94
Dave <HSC.Coach@gmail.com>
2020-11-08 21:57:08 -0500

    changed almost everything, most features working now

--- src/js/DonutMaker.js
+++ src/js/DonutMaker.js
@@ -1,89 +1,5 @@
 class DonutMaker {
-//   constructor() {
-//     this._donutCount = 1000;
-//     this._numberOfAutoClickers = 0;
-//     this._autoClickerCost = 100;
-//     this._donutMultiplierCount = 0;
-//     this._donutMultiplierCost = 10;
-//   }
 
-//   stashDonutsForTesting() {
-//     this._donutCount += 1000;
-//   }
-
-//   oneAutoClickerToTest() {
-//     this._numberOfAutoClickers += 1;
-//   }
-
-//   recordClick() {
-//     if (this.donutMultiplierCount > 0) {
-//       this._donutCount += Math.pow(1.2, this._donutMultiplierCount);
-//     } else {
-//       this._donutCount++;
-//     }
-//   }
-//   get clickCount() {
-//     return this._donutCount;
-//   }
-//   recordAutoClick() {
-//     var i = 0;
-//     while (i < this._numberOfAutoClickers) {
-//       this._donutCount += Math.pow(1.2, this._donutMultiplierCount);
-//       i++;
-//     }
-//   }
-//   get autoClickCount() {
-//     return this._donutCount;
-//   }
-
-//   get autoClickerCost() {
-//     let unRoundedCost = 100 * Math.pow(1.1, this._numberOfAutoClickers - 1);
-//     this._autoClickerCost = Math.round(unRoundedCost);
-//     return this._autoClickerCost;
-//   }
-
-//   buyAutoClicker() {
-//     if (this.autoClickersPurchased == 0) {
-//       if (this._donutCount >= 100) {
-//         this._numberOfAutoClickers++;
-//         this._donutCount -= 100;
-//       } else {
-//       }
-//     } else {
-//       if (this._donutCount >= this.autoClickerCost) {
-//         this._numberOfAutoClickers++;
-//         this._donutCount -= this.autoClickerCost;
-//       } else {
-//       }
-//     }
-//   }
-
-//   get autoClickersPurchased() {
-//     return this._numberOfAutoClickers;
-//   }
-
-//   activateAutoClickers() {
-//     var i = 0;
-//     while (i < this._numberOfAutoClickers) {
-//       this._donutCount++;
-//       i++;
-//     }
-//   }
-
-//   buyDonutMultiplier() {
-//     if (this._donutCount >= this._donutMultiplierCost) {
-//       this._donutMultiplierCount++;
-//       this._donutCount -= this._donutMultiplierCost;
-//       this._donutMultiplierCost =
-//         this._donutMultiplierCost + this._donutMultiplierCost * 0.1;
-//     }
-//   }
-//   get donutMultiplierCount() {
-//     return this._donutMultiplierCount;
-//   }
-//   get donutMultiplierCost() {
-//     return this._donutMultiplierCost;
-//   }
 
 constructor() {
   this._donutCount = 0; 
@@ -105,12 +21,10 @@ get donutCount() {
   }
 
 
-//Auto Clicker
+
 buyAutoClicker() {
       if (this._donutCount >= this._autoClickerCost) {
-          // logic for when an auto clicker is bought
           this._donutCount -= this._autoClickerCost;
-          // this._donutCount = this._donutCount - this._autoClickerCost;
           this._autoClickerCount++;
           this._autoClickerCost = this._autoClickerCost + (this._autoClickerCost * 0.1);
           this._autoClickerCost = Math.round(this._autoClickerCost);
@@ -128,7 +42,7 @@ activateAutoClickers() {
 }
 
 
-//Donut Multiplier
+
 buyDonutMultiplier() {
   if (this.donutCount >= this._donutMultiplierCost) {
       this._donutMultiplierCount++;
@@ -144,7 +58,7 @@ get donutMultiplierCost() {
   return this._donutMultiplierCost;
 }
 
-//Game Reset
+
 resetGame() {
   this._donutCount = 0;
   this._autoClickerCount = 0;
--- src/js/app.js
+++ src/js/app.js
@@ -3,131 +3,6 @@ import {
 } from "/src/js/DonutMaker.js ";
 
 const donutMaker = new DonutMaker();
-
-
-
-//     const updateDonutCount = function(donutMaker) {
-//         const donutCount = document.querySelector(".donut-count");
-//         donutCount.innerText =  Math.round(donutMaker.donutCount * 10)/10
-//         updateDonutMultiplierButton(donutMaker);
-//         updateAutoClickerButton(donutMaker);
-
-//     };
-
-//     const donutButton = document.querySelector(".donut-button");
-// donutButton.addEventListener("click", () => {
-//     // console.log('Donut Button Clicked')
-//     donutMaker.recordClick();
-//     updateDonutCount(donutMaker);
-
-// });
-
-// const updateAutoClickerCount = function(donutMaker) {
-//     const autoClikcerCount = document.querySelector(".auto-clicker-count");
-//     autoCLickerCount.innerText = donutMaker.autoCLickerCount;
-
-// };
-
-// const purchaseAutoClickerButton = document.querySelector(".purchase-auto-clicker-button");
-//     purchaseAutoClickerButton.addEventListener("click", () => {
-//     donutMaker.buyAutoClicker();
-//     updateNumberOfAutoClickers(donutMaker);
-//     updateDonutCount(donutMaker);
-//     // displayNumberOfAutoClickers(userDonut)
-//     displayAutoClickerCost(donutMaker);
-// });
-
-// const displayAutoClickerCost = function(donutMaker) {
-//     const autoClickerCost = document.querySelector(".auto-clicker-cost");
-//     autoClickerCost.innerText = "Cost: " + donutMaker.autoClickerCost + " Donuts";
-// }
-
-// function updateAutoClickerButton(donutMaker){
-//     const lightUpPurchaseAutoClickerButton = document.querySelector(".buy-auto-button"); //.purchase-auto-clicker-button
-//     lightUpPurchaseAutoClickerButton.classList.add('grey-out-auto-clicker-button');
-//     if(donutMaker.donutCount < donutMaker.autoClickerCost){
-//         lightUpPurchaseAutoClickerButton.classList.toggle('grey-out-auto-clicker-button');
-//     }
-// };
-
-// const updateDonutMultiplierCount = function(donutMaker) {
-//     const donutMultiplierCount = document.querySelector(".multiplier-count"); //.donut-multiplier-count
-//     donutMultiplierCount.innerText = donutMaker.donutMultiplierCount;
-// }
-
-// const purchaseDonutMultiplierButton = document.querySelector(".multiplier-button"); //.purchase-donut-multiplier-button
-// purchaseDonutMultiplierButton.addEventListener("click", () => {
-//     donutMaker.buyDonutMultiplier();
-//     updateDonutCount(donutMaker);
-//     updateDonutMultiplierCount(donutMaker);
-//     displayDonutMultiplierCost(donutMaker);
-//     // displayClickValue(donutMaker);
-// });
-
-// const displayDonutMultiplierCost = function(donutMaker) {
-//     const displayDonutMultiplierCost = document.querySelector(".multiplier-cost"); //.donut-multiplier-cost
-//     donutMultiplierCost.innerText = "Cost: " + Math.round(donutMaker.donutMultiplierCost * 10) / 10 + " Donuts";
-// }
-
-// function updateDonutMultiplierButton(donutMaker) {
-//     const lightUpPurchaseDonutMultiplierButton = document.querySelector(".purchase-donut-multiplier-button")//semicolon
-//     lightUpPurchaseDonutMultiplierButton.classList.add('gray-out-donut-multiplier-button');
-//     if (donutMaker.donutCount < donutMaker.donutMultiplierCost) {
-//         lightUpPurchaseDonutMultiplierButton.classList.toggle('gray-out-donut-multiplier-button');
-//     }
-// }
-// //Modal
-// const developerInfoButton = document.querySelector('.developer-button');
-// const modal = document.querySelector('.modal');
-// const backdrop = document.querySelector('.backdrop');
-
-// function closeModal() {
-//     modal.style.display = 'none';
-//     backdrop.style.display = 'none';
-// }
-
-// developerInfoButton.addEventListener("click", function () {
-//     modal.style.display = 'block';
-//     backdrop.style.display = 'block';
-// });
-
-// const exitButton = document.querySelector(".exit-button");
-// exitButton.addEventListener('click', closeModal);
-// backdrop.addEventListener('click', closeModal);
-
-// //Modal2
-// const aboutUsButton = document.querySelector('.about-us-button');
-// const modal2 = document.querySelector('.modal2');
-// const backdrop2 = document.querySelector('.backdrop2');
-
-
-// function closeModal2() {
-//     modal2.style.display = 'none';
-//     backdrop2.style.display = 'none';
-// }
-
-// aboutUsButton.addEventListener("click", function () {
-//     modal2.style.display = 'block';
-//     backdrop2.style.display = 'block';
-// });
-
-// const exitButton2 = document.querySelector(".exit-button2");
-// exitButton2.addEventListener('click', closeModal2);
-// backdrop2.addEventListener('click', closeModal2);
-
-
-// //Reset Button
-// const activateResetGame = document.querySelector(".reset-game");
-// activateResetGame.addEventListener("click", () => {
-//     donutMaker.resetGame();
-//     updateAutoClickerCount(donutMaker);
-//     updateDonutCount(donutMaker);
-//     displayAutoClickerCost(donutMaker);
-//     updateDonutMultiplierCount(donutMaker);
-//     updateDonutCount(donutMaker);
-//     displayDonutMultiplierCost(donutMaker);
-// });
-
 const updateDonutCount = function (donutMaker) {
     const donutCount = document.querySelector(".donut-count");
     donutCount.innerText = Math.round(donutMaker.donutCount * 10) / 10
@@ -142,7 +17,7 @@ donutButton.addEventListener("click", () => {
 });
 
 
-//Auto Clickers
+
 const updateAutoClickerCount = function (donutMaker) {
     const autoClickerCount = document.querySelector(".auto-clicker-count");
     autoClickerCount.innerText = donutMaker.autoClickerCount;
@@ -170,7 +45,7 @@ function updateAutoClickerButton(donutMaker) {
 };
 
 
-//Donut Multipliers
+
 const updateDonutMultiplierCount = function (donutMaker) {
     const donutMultiplierCount = document.querySelector(".donut-multiplier-count");
     donutMultiplierCount.innerText = donutMaker.donutMultiplierCount;
@@ -197,7 +72,7 @@ function updateDonutMultiplierButton(donutMaker) {
         lightUpPurchaseDonutMultiplierButton.classList.toggle('gray-out-donut-multiplier-button');
     }
 }
-//Modal
+
 const developerInfoButton = document.querySelector('.developer-button');
 const modal = document.querySelector('.modal');
 const backdrop = document.querySelector('.backdrop');
@@ -216,7 +91,7 @@ const exitButton = document.querySelector(".exit-button");
 exitButton.addEventListener('click', closeModal);
 backdrop.addEventListener('click', closeModal);
 
-//Modal2
+
 const aboutUsButton = document.querySelector('.about-us-button');
 const modal2 = document.querySelector('.modal2');
 const backdrop2 = document.querySelector('.backdrop2');
@@ -237,7 +112,7 @@ exitButton2.addEventListener('click', closeModal2);
 backdrop2.addEventListener('click', closeModal2);
 
 
-//Reset Button
+
 const activateResetGame = document.querySelector(".reset-game");
 activateResetGame.addEventListener("click", () => {
     donutMaker.resetGame();
